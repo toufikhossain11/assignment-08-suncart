1d0706191e4886e6fa8fa13f8f25b74b87198a62
toufikhossain11 <toufikhossain546@gmail.com>
2026-05-03 00:06:58 +0600

    fix banner or card

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,33 +1,32 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaStar } from "react-icons/fa";
 
 const ProductCard = ({ data }) => {
     return (
         <div>
-            <div className="card w-80 h-70 p-3  bg-white border border-orange-100 shadow-md hover:shadow-xl hover:-translate-y-2 transition-all text-center">
-                <div className="relative w-full aspect-auto">
+            <div className="p-3 w-76  h-106  bg-white border border-orange-100 shadow-md hover:shadow-xl hover:-translate-y-2 transition-all text-center rounded-xl">
+                <Link href={`/products/${data.id}`} className="relative w-full aspect-square">
                     <Image
                         src={data.image}
-                        width={400}
-                        height={500}
-                        alt="Picture of the author"
-                    />
-                    {/* <Image
-                        src={data.image}
-                        alt="banner"
-                        fill
-                        loading="eager"
+                        width={300}
+                        height={100}
+                        alt={data.name}
                         className="object-cover rounded-xl"
-                    /> */}
-                </div>
-                <div className="card-body">
-                    <h2 className="font-bold text-xl text-slate-900">{data.name}</h2>
-                    <div className="grid grid-cols-2 justify-between">
-                        <div>
+                    />
+                </Link>
+                <div className="p-3">
+                    <Link href={`/products/${data.id}`}>
+                        <h2 className="font-bold text-xl text-slate-900">{data.name}</h2>
+                    </Link>
+                    <div className="grid grid-cols-2 items-center justify-between gap-14">
+                        
                             <p className="text-orange-500 font-bold">$ {data.price}</p>
+                       
+                        <div className="flex items-center gap-2 bg-yellow-50 px-4 py-2 rounded-full w-fit">
+                            <FaStar className="text-yellow-400 text-lg" />
+                            <span className="font-semibold text-slate-700">{data.rating}</span>
                         </div>
-
-                        <p className="text-yellow-400 font-bold"> {data.rating}</p>
 
                     </div>
                     <div className="">
